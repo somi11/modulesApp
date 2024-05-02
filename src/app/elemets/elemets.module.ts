0596aeff73441fc344d81394a57c69cac2555b40
somi11 <somisun41@gmail.com>
2024-05-02 11:36:04 +0500

    first commit

---
+++ src/app/elemets/elemets.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElemetsRoutingModule } from './elemets-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ElemetsRoutingModule
+  ]
+})
+export class ElemetsModule { }
